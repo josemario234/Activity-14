98f3583241752f444efa1047b32e025f1ff9c035
josemario234 <AL02963295@tecmilenio.mx>
2026-05-04 20:43:49 -0700

    Define which attributes can be mass assigned in "Note.php" file

--- API-app/app/Models/Note.php
+++ API-app/app/Models/Note.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Note extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'author',
+        'body',
+        'classification',
+    ];
 }
